fde6c67f3a7d8f860b8f3f424d673f9174d7c215
Nabil Canan <NCanan@sager.com>
2023-11-07 16:15:48 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,189 @@
+import os
+import tkinter as tk
+from threading import Thread
+from tkinter import filedialog, messagebox, ttk
+import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from tkinterdnd2 import TkinterDnD, DND_FILES
+from vlookup import perform_vlookup
+import pygame
+
+
+# plays last Christmas when the gui is opened lol
+def play_background_music():
+    pygame.mixer.init()
+    pygame.mixer.music.load('images-videos/last_christmas.mp3')
+    pygame.mixer.music.play(-1)  # Play the music, -1 means play indefinitely in loop
+    pass
+
+
+music_thread = Thread(target=play_background_music, daemon=True)
+music_thread.start()
+
+
+def sort_excel(file_path, sort_columns, ascending_order, file_type=""):
+    if not sort_columns:
+        messagebox.showerror("Error", "No columns selected for sorting.")
+        return
+
+    try:
+        # Read the Excel file into a pandas DataFrame
+        df = pd.read_excel(file_path)
+
+        # If 'SND Cost' is one of the sort columns, convert it to numeric
+        if 'SND Cost' in sort_columns:
+            df['SND Cost'] = pd.to_numeric(df['SND Cost'], errors='coerce')
+
+        # If 'VPC Cost' is one of the sort columns, convert it to numeric
+        if 'VPC Cost' in sort_columns:
+            df['VPC Cost'] = pd.to_numeric(df['VPC Cost'], errors='coerce')
+
+        # Sort the DataFrame based on the selected columns
+        df = df.sort_values(by=sort_columns, ascending=ascending_order)
+
+        # Save the sorted DataFrame back to the Excel file
+        df.to_excel(file_path, index=False)
+        #
+        # messagebox.showinfo("Success!", f"Success! {file_type} file sorted and saved successfully.")
+
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+
+def process_file(file_path):
+    filename = os.path.basename(file_path).lower()
+    if 'award' in filename:
+        sort_excel(file_path, ['Product ID', 'Award Cust ID'], [True, False], "Award")
+    elif 'backlog' in filename:
+        sort_excel(file_path, ['Product ID', 'Backlog Entry'], [True, False], "Backlog")
+    elif 'sales' in filename:
+        sort_excel(file_path, ['Product ID', 'Last Ship Date'], [True, False], "Sales History")
+    elif 'snd' in filename:
+        sort_excel(file_path, ['Product ID', 'SND Cost'], [True, True], "Ship & Debit")
+    elif 'vpc' in filename:
+        sort_excel(file_path, ['PART ID', 'VPC Cost'], [True, False], "VPC")
+
+
+def merge_files_and_create_lost_items(folder_path):
+    print("Merging files and creating lost items...")
+
+    # Define the order and corresponding keywords in which the files should be merged
+    merge_order = [
+        ('Awards', 'award'),
+        ('Backlog', 'backlog'),
+        ('Sales History', 'sales'),
+        ('SND', 'snd'),
+        ('VPC', 'vpc')
+    ]
+
+    # Find all Excel files in the folder
+    excel_files = [f for f in os.listdir(folder_path) if f.endswith('.xlsx')]
+
+    # Extracting contract files based on naming convention and dates in filenames
+    contract_files = sorted([f for f in excel_files if 'CT ACTIVE CONTRACTS' in f],
+                            key=lambda x: x[-12:])  # Sort by date at the end of filename
+
+    if len(contract_files) < 2:
+        messagebox.showerror("Error", "Not enough contract files found. Need current and previous week's files.")
+        return
+
+    # Assume the last two files are current and previous based on naming convention
+    current_contract_path = os.path.join(folder_path, contract_files[-1])
+    prev_contract_path = os.path.join(folder_path, contract_files[-2])
+
+    # Load the workbooks and dataframes for the current and previous contracts
+    active_workbook = load_workbook(current_contract_path)
+    active_df = pd.read_excel(current_contract_path, sheet_name=0, header=1)
+    prev_df = pd.read_excel(prev_contract_path, sheet_name=0, header=0)
+
+    # Now we have the dataframes, we need to check if 'IPN' column exists
+    if 'IPN' not in prev_df.columns or 'IPN' not in active_df.columns:
+        messagebox.showerror("Error", "'IPN' column is missing in one of the contract files.")
+        return
+
+    # Create a 'Lost Items' sheet
+    lost_items_sheet = active_workbook.create_sheet('Lost Items')
+    lost_items_df = prev_df[~prev_df['IPN'].isin(active_df['IPN'])]
+    if not lost_items_df.empty:
+        for r in dataframe_to_rows(lost_items_df, index=False, header=True):
+            lost_items_sheet.append(r)
+
+    # Create a 'Prev Contract' sheet
+    prev_contract_sheet = active_workbook.create_sheet('Prev Contract')
+    for r in dataframe_to_rows(prev_df, index=False, header=True):
+        prev_contract_sheet.append(r)
+
+    # Loop through the files in the specified order and merge them
+    for sheet_name, keyword in merge_order:
+        for file_name in excel_files:
+            if keyword.lower() in file_name.lower() and file_name not in contract_files:
+                file_path = os.path.join(folder_path, file_name)
+                data_df = pd.read_excel(file_path, sheet_name=0)
+                new_sheet = active_workbook.create_sheet(title=sheet_name)
+                for r in dataframe_to_rows(data_df, index=False, header=True):
+                    new_sheet.append(r)
+                break  # Once the correct file is found and processed, break the loop
+
+    # Save the active workbook with the new sheets
+    try:
+        active_workbook.save(current_contract_path)
+        messagebox.showinfo("Success!", "Files merged and sheets created successfully in order.")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+
+def on_drop(event):
+    folder_path = event.data
+    if os.path.isdir(folder_path):
+        process_folder(folder_path)
+        messagebox.showinfo("Complete", "Files have been processed.")
+    else:
+        messagebox.showerror("Error", "Please drop a folder, not files.")
+
+
+def process_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path) and filename.endswith('.xlsx'):
+            process_file(file_path)
+    # After all files have been processed, proceed to merge and create the lost items sheet
+    merge_files_and_create_lost_items(folder_path)
+
+
+def setup_gui(root):
+    style = ttk.Style()
+    style.configure("TButton", font=("Arial", 16, "bold"), width=40, height=2)
+    style.map("TButton", foreground=[('active', 'red')], background=[('active', 'blue')])
+    style.configure("TButton", background="white")
+
+    title_label = ttk.Label(root, text="Welcome Partnership Team!",
+                            font=("Arial", 32, "underline"), background="white", foreground="#103d81")
+    title_label.pack(pady=10)
+
+    description_label = ttk.Label(root,
+                                  text="This tool allows you to sort your Excel files for our Creation \n"
+                                       "Contact Automatically once saved in your desired folder",
+                                  font=("Rupee", 18, "underline"), background="white", anchor="center",
+                                  justify="center", )
+    description_label.pack(pady=10)
+
+    # Create a label that will act as the drop area
+    drop_area = tk.Label(root, text='Drop Folder Here', pady=50, padx=50, relief="groove", borderwidth=2)
+    drop_area.pack(pady=30, padx=30)  # Add some padding to make the border visible
+
+    perform_vlookup_button = ttk.Button(root, text="Perform VLook-Up to new file",
+                                        command=lambda: perform_vlookup(perform_vlookup_button), style="TButton")
+    perform_vlookup_button.pack(pady=10)
+
+    # Register label as a drop target
+    drop_area.drop_target_register(DND_FILES)
+    drop_area.dnd_bind('<<Drop>>', on_drop)
+
+
+if __name__ == "__main__":
+    root = TkinterDnD.Tk()
+    root.title('File Processor')
+    root.geometry('800x600')  # Set the window size to be bigger
+    setup_gui(root)
+    root.mainloop()
---
+++ vlookup.py
@@ -0,0 +1,205 @@
+from datetime import datetime
+from tkinter import filedialog, messagebox
+import pandas as pd
+from openpyxl.styles import PatternFill, Alignment
+import numpy as np
+from openpyxl.utils import get_column_letter
+from colored_headers import headers_to_color
+
+
+def perform_vlookup(button_to_disable):
+    try:
+        # Ask the user for the contract file
+        contract_file = filedialog.askopenfilename(title="Select the contract file, where we need a vlookup",
+                                                   initialdir="P:\Partnership_Python_Projects\Creation\test_001")
+
+        # Load 'Active Supplier Contracts' and 'Prev Contract' sheets
+        active_supplier_df = pd.read_excel(contract_file, sheet_name='Active Supplier Contracts', header=1)
+        prev_contract_df = pd.read_excel(contract_file, sheet_name='Prev Contract', header=0)
+        lost_items_df = pd.read_excel(contract_file, sheet_name='Lost Items')
+        awards_df = pd.read_excel(contract_file, sheet_name='Awards')
+        snd_df = pd.read_excel(contract_file, sheet_name='SND')
+        vpc_df = pd.read_excel(contract_file, sheet_name='VPC')
+        backlog_df = pd.read_excel(contract_file, sheet_name='Backlog')
+        sales_history_df = pd.read_excel(contract_file, sheet_name='Sales History')
+
+        print("Loaded 'Active Supplier Contracts' sheet with shape:", active_supplier_df.shape)
+        print("Loaded 'Prev Contract' sheet with shape:", prev_contract_df.shape)
+
+        # Drop the 'LW PRICE' column if it exists in the 'Prev Contract' dataframe
+        if 'LW PRICE' in prev_contract_df.columns:
+            prev_contract_df.drop('LW PRICE', axis=1, inplace=True)
+
+        # Rename the 'Price' column from 'Prev Contract' dataframe to 'LW PRICE'
+        prev_contract_df.rename(columns={'Price': 'LW PRICE'}, inplace=True)
+
+        # merge
+        active_supplier_df = active_supplier_df.merge(
+            prev_contract_df[
+                ['IPN', 'LW PRICE', 'PSoft Part', "Prev Contract MPN", "MPN Match",
+                 "Price Match MPN",
+                 "Contract Change", "count",
+                 "Corrected PSID Ct", "SUM", "AVG", "DIFF", "PSID All Contract Prices Same?",
+                 "PS Award Price", "PS Award Exp Date", "PS Awd Cust ID", "Price Match Award",
+                 "Corp Awd Loaded", "90 DAY PI - NEW PRICE", "PI SENT DATE",
+                 "DIFF Price Increase", "PI EFF DATE", "12 Month CPN Sales", "GP%", "Cost",
+                 "Cost Note", "Quote#", "Cost Exp Date", "Cost MOQ",
+                 "Review Note", "LW Cost", "LW Quote#", "LW Cost Exp Date", "LW Review Note"]], on='IPN', how='left')
+
+        # Calculate the counts for each 'PSoft Part'
+        psoft_part_counts = active_supplier_df['PSoft Part'].value_counts()
+
+        # Update the 'count' column in active_supplier_df with the new counts
+        active_supplier_df['count'] = active_supplier_df['PSoft Part'].map(psoft_part_counts)
+
+        # Iterate through each row in the active_supplier_df to look for a match in SND and VPC
+        for idx, row in active_supplier_df.iterrows():
+            psoft_part = row['PSoft Part']
+
+            # Check SND using the 'Product ID' column
+            matching_snd = snd_df[snd_df['Product ID'] == psoft_part]
+            if not matching_snd.empty and not pd.isna(matching_snd.iloc[0, 1]):
+                active_supplier_df.at[idx, 'Cost'] = matching_snd.iloc[0, 1]
+                continue  # If found in SND, skip checking VPC for the same PSoft Part
+
+            # Check VPC using the 'PART ID' column
+            matching_vpc = vpc_df[vpc_df['PART ID'] == psoft_part]
+            if not matching_vpc.empty and not pd.isna(matching_vpc.iloc[0, 1]):
+                active_supplier_df.at[idx, 'Cost'] = matching_vpc.iloc[0, 1]
+
+        print(active_supplier_df.columns)
+
+        # The Contract Change comparison is done between 'Price' and 'LW PRICE'.
+        # tolerance = 0.01  # you can set it to any value you deem fit
+
+        print("Shape of active_supplier_df['Price']:", active_supplier_df['Price'].shape)
+        print("Shape of active_supplier_df['LW PRICE']:", active_supplier_df['LW PRICE'].shape)
+
+        active_supplier_df['Contract Change'] = np.where(
+            active_supplier_df['LW PRICE'].isna(),  # Check if 'LW PRICE' is NaN or null
+            'New Item',
+            np.where(
+                active_supplier_df['Price'] == active_supplier_df['LW PRICE'],
+                'No Change',
+                np.where(
+                    active_supplier_df['Price'] > active_supplier_df['LW PRICE'],
+                    'Price Increase',
+                    'Price Decrease'  # Since we've covered all other scenarios, this can be the else condition.
+                )
+            )
+        )
+
+        # Ask the user for the output file path
+        output_file = filedialog.asksaveasfilename(defaultextension=".xlsx", title="Save the output file as",
+                                                   initialdir="P:\Partnership_Python_Projects\Creation\test_001")
+
+        # Write all the DataFrames to the new Excel file in the specified order
+        if output_file:
+            with (pd.ExcelWriter(output_file, engine='openpyxl') as writer):
+                active_supplier_df.to_excel(writer, index=False, sheet_name='Active Supplier Contracts')
+                prev_contract_df.to_excel(writer, index=False, sheet_name='Prev Contract')
+                lost_items_df.to_excel(writer, index=False, sheet_name='Lost Items')
+                awards_df.to_excel(writer, index=False, sheet_name='Awards')
+                snd_df.to_excel(writer, index=False, sheet_name='SND')
+                vpc_df.to_excel(writer, index=False, sheet_name='VPC')
+                backlog_df.to_excel(writer, index=False, sheet_name='Backlog')
+                sales_history_df.to_excel(writer, index=False, sheet_name='Sales History')
+
+                # Grabbing the workbook and the desired sheet
+                workbook = writer.book
+                sheet = workbook['Active Supplier Contracts']
+
+                # Freeze the top row
+                sheet.freeze_panes = 'A2'
+
+                # Freeze the column H2
+                sheet.freeze_panes = "H2"
+
+                # Turn on filters for the top row only
+                sheet.auto_filter.ref = sheet.dimensions
+
+                # Wrap text for the first row, makes it look neater
+                for cell in sheet["1:1"]:
+                    cell.alignment = Alignment(wrap_text=True)
+
+                # Define the columns for 'Price_x', 'Cost', 'GP%', 'Cost Exp Date', 'Award Date', and 'Last Update Date'
+                price_x_col, cost_col, gp_col, date_col, award_date_col, last_update_date_col, pi_sent_date_col = None, None, None, None, None, None, None
+
+                for col_num, col_cells in enumerate(sheet.columns, start=1):
+                    col_val = col_cells[0].value  # header value in current column
+                    if col_val == 'Price':
+                        price_x_col = col_num
+                    elif col_val == 'Cost':
+                        cost_col = col_num
+                    elif col_val == 'GP%':
+                        gp_col = col_num
+                    elif col_val == 'Cost Exp Date':
+                        date_col = col_num
+                    elif col_val == 'Award Date':
+                        award_date_col = col_num
+                    elif col_val == 'Last Update Date':
+                        last_update_date_col = col_num
+                    elif col_val == 'PI SENT DATE':
+                        pi_sent_date_col = col_num
+
+                    # Formatting for the "PI SENT DATE" column
+                    if pi_sent_date_col:
+                        for row in range(2, sheet.max_row + 1):
+                            pi_sent_date_cell = f"{get_column_letter(pi_sent_date_col)}{row}"
+
+                            cell_value = sheet[pi_sent_date_cell].value
+
+                            # If cell value is '-2' or is an out-of-range date
+                            if cell_value == "-2" or str(cell_value) == "####" or cell_value is None:
+                                sheet[pi_sent_date_cell].value = ""
+                            else:
+                                try:
+                                    # Check if the value can be interpreted as a date
+                                    parsed_date = datetime.strptime(str(cell_value), "%Y-%m-%d %H:%M:%S")
+                                    if parsed_date.year < 1900 or parsed_date.year > 9999:  # Excel's date limits
+                                        sheet[pi_sent_date_cell].value = ""
+                                except ValueError:
+                                    sheet[pi_sent_date_cell].value = ""
+
+                    # Now apply the date format
+                    if pi_sent_date_col:
+                        for row in range(2, sheet.max_row + 1):
+                            pi_sent_date_cell = f"{get_column_letter(pi_sent_date_col)}{row}"
+                            sheet[pi_sent_date_cell].number_format = 'MM/DD/YYYY'  # Format as MM/DD/YYYY
+
+                # Check if all the required columns were found and apply formatting
+                if all([price_x_col, cost_col, gp_col, date_col, award_date_col, last_update_date_col, pi_sent_date_col]):
+                    for row in range(2, sheet.max_row + 1):  # Assuming row 1 is the header, so we start from row 2
+                        gp_cell = f"{get_column_letter(gp_col)}{row}"
+                        price_x_cell = f"{get_column_letter(price_x_col)}{row}"
+                        cost_cell = f"{get_column_letter(cost_col)}{row}"
+                        date_cell = f"{get_column_letter(date_col)}{row}"
+                        award_date_cell = f"{get_column_letter(award_date_col)}{row}"
+                        last_update_date_cell = f"{get_column_letter(last_update_date_col)}{row}"
+                        pi_sent_date_cell = f"{get_column_letter(pi_sent_date_col)}{row}"
+
+                        # Format the cells
+                        sheet[price_x_cell].number_format = '$0.0000'
+                        sheet[gp_cell].number_format = '0.00%'  # GP% as percent
+                        sheet[cost_cell].number_format = '$0.0000'  # Cost as dollar with four decimal places
+                        sheet[date_cell].number_format = 'MM/DD/YYYY'  # Date as MM/DD/YYYY
+                        sheet[award_date_cell].number_format = 'MM/DD/YYYY'  # Award Date as MM/DD/YYYY
+                        sheet[last_update_date_cell].number_format = 'MM/DD/YYYY'  # Last Update Date as MM/DD/YYYY
+                        sheet[pi_sent_date_cell].number_format = 'MM/DD/YYYY'  # Format as MM/DD/YYYY
+
+                        # Apply formula to GP%
+                        formula = f"=IF({price_x_cell}=0,0,({price_x_cell} - {cost_cell}) / {price_x_cell})"
+                        sheet[gp_cell] = formula
+
+                for col_num, col_cells in enumerate(sheet.columns, start=1):
+                    if col_cells[0].value in headers_to_color:
+                        col_cells[0].fill = PatternFill(start_color=headers_to_color[col_cells[0].value],
+                                                        end_color=headers_to_color[col_cells[0].value],
+                                                        fill_type="solid")
+
+            # Display a success message in a message box
+            messagebox.showinfo("Success", "The output file has been saved as: " + output_file)
+            button_to_disable.config(state="disabled")
+
+    except Exception as e:
+        messagebox.showerror("Error Process was Cancelled", str(e))
